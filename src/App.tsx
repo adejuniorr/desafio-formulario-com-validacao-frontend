8a77972c26d33ca87d55d4fc4dcb2301656242b5
Ademir Júnior <adejr0220@gmail.com>
2024-10-19 17:22:22 -0300

    :sparkles: install and init TailwindCSS
    Also update index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 function App() {
-
   return (
     <>
-      <main>
+      <main className="h-screen w-screen flex items-center justify-center bg-gray-900 text-gray-100">
         <h1>Hello World</h1>
       </main>
     </>
   );
 }
 
-export default App
+export default App;
