8d33add24c02e98cb5394daec083b7398f219477
Ademir Júnior <adejr0220@gmail.com>
2024-10-20 14:21:50 -0300

    :arrow-up: :lipstick: update & style EmailInput

--- src/components/form/EmailInput.tsx
+++ src/components/form/EmailInput.tsx
@@ -2,6 +2,11 @@ interface EmailInputProps {}
 
 export const EmailInput = ({}: EmailInputProps) => {
   return (
-    <div>EmailInput</div>
-  )
-}
+    <input
+      type="email"
+      title="Digite seu e-mail"
+      placeholder="E-mail"
+      className="w-full p-2 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 dark:bg-gray-900 dark:text-gray-200 dark:border-black dark:focus:ring-cyan-300"
+    />
+  );
+};
