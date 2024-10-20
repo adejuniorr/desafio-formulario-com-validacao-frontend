31473e94144a12a4865f8ae325c0263daabd5b94
Ademir Júnior <adejr0220@gmail.com>
2024-10-20 14:22:23 -0300

    :arrow-up: :lipstick: update & style PhoneNumberInput

--- src/components/form/PhoneNumberInput.tsx
+++ src/components/form/PhoneNumberInput.tsx
@@ -2,6 +2,11 @@ interface PhoneNumberInputProps {}
 
 export const PhoneNumberInput = ({}: PhoneNumberInputProps) => {
   return (
-    <div>PhoneNumberInput</div>
-  )
-}
+    <input
+      type="number"
+      title="Digite seu número de telefone"
+      placeholder="Telefone"
+      className="w-full p-2 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 dark:bg-gray-900 dark:text-gray-200 dark:border-black dark:focus:ring-cyan-300"
+    />
+  );
+};
