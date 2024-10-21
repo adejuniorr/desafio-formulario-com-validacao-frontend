6ba609d30da7f78b977f176d3cd9da3e29367201
Ademir Júnior <adejr0220@gmail.com>
2024-10-21 16:34:02 -0300

    :lipstick: update Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,7 +8,7 @@ interface FormProps {}
 
 export const Form = ({}: FormProps) => {
   return (
-    <div className="bg-fusion-dark-blue rounded-md shadow shadow-gray-700 max-w-[500px] w-full flex flex-col gap-4 px-4 py-6">
+    <div className="bg-fusion-dark-blue rounded-md shadow shadow-gray-700 max-w-[500px] w-full flex flex-col gap-4 px-4 py-6 mb-10">
       <h1 className="font-extrabold text-[2rem] w-[60%] mb-4">
         Preencha com seus dados
       </h1>
