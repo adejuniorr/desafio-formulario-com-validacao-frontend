141cd738f11d03f0aaca987fabdb621ae070656e
Ademir Júnior <adejr0220@gmail.com>
2024-10-21 15:42:16 -0300

    :sparkles: add Fusion logotype

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import { Form } from "./components/Form";
 function App() {
   return (
     <>
-      <main className="h-screen w-screen flex items-center justify-center bg-gray-400 text-gray-100">
+      <main className="min-h-[100vh]  px-4 pt-10 flex flex-col gap-4 items-center bg-gray-800 text-gray-100">
+        <img
+          src="/fusion-logo.svg"
+          alt="Logotipo do Projeto Frontend Fusion"
+          className="mx-auto max-w-[300px] w-10 min-w-[130px]"
+        />
         <Form />
       </main>
     </>
