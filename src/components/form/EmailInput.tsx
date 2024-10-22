d48fee8cc9aa3cb64ef9404911ae8a71a00d6b94
Ademir Júnior <adejr0220@gmail.com>
2024-10-22 14:18:57 -0300

    :recycle: refact EmailInput
    Apply react-hook-form configs to component

--- src/components/form/EmailInput.tsx
+++ src/components/form/EmailInput.tsx
@@ -1,13 +1,23 @@
-interface EmailInputProps {}
+import { useFormContext } from "react-hook-form";
 
-export const EmailInput = ({}: EmailInputProps) => {
+export const EmailInput = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
   return (
-    <input
-      required
-      type="email"
-      title="Digite seu e-mail"
-      placeholder="E-mail"
-      className="w-full p-3 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 dark:bg-gray-900 dark:text-gray-200 dark:border-black dark:focus:ring-fusion-cyan"
-    />
+    <>
+      <input
+        {...register("email")}
+        type="email"
+        title="Digite seu e-mail"
+        placeholder="E-mail"
+        className="w-full p-3 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 dark:bg-gray-900 dark:text-gray-200 dark:border-black dark:focus:ring-fusion-cyan"
+      />
+      {errors.email && (
+        <span className="text-red-500 text-sm">{errors.email?.message as string}</span>
+      )}
+    </>
   );
 };
