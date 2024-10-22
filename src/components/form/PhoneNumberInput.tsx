e8dac5481e75039d8105cb88f1ab78d9ae66c23e
Ademir Júnior <adejr0220@gmail.com>
2024-10-22 14:18:38 -0300

    :recycle: refact PhoneNumberInput
    Apply react-hook-form configs to component

--- src/components/form/PhoneNumberInput.tsx
+++ src/components/form/PhoneNumberInput.tsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { useEffect } from "react";
+import { useFormContext } from "react-hook-form";
 
-interface PhoneNumberInputProps {}
-
-export const PhoneNumberInput = ({}: PhoneNumberInputProps) => {
-  const [number, setNumber] = useState<string>("");
+export const PhoneNumberInput = () => {
+  // const [number, setNumber] = useState<string>("");
+  const {
+    register,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useFormContext();
+  const number = watch("phone");
 
   const formatPhoneNumber = (value: string) => {
     const cleanValue = value.replace(/\D/g, "");
 
-    if (cleanValue.length <= 2) {
+    if (cleanValue.length !== 0 && cleanValue.length <= 2) {
       return `(${cleanValue}`;
-    } else if (cleanValue.length <= 6) {
+    } else if (cleanValue.length !== 0 && cleanValue.length <= 6) {
       return `(${cleanValue.slice(0, 2)}) ${cleanValue.slice(2)}`;
-    } else {
+    } else if (cleanValue.length !== 0) {
       return `(${cleanValue.slice(0, 2)}) ${cleanValue.slice(
         2,
         7
@@ -20,13 +27,18 @@ export const PhoneNumberInput = ({}: PhoneNumberInputProps) => {
     }
   };
 
+  useEffect(() => {
+    setValue("phone", formatPhoneNumber(number || ""));
+  }, [number, setValue]);
+
   const handleTypePhoneNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
     const input = e.target.value;
 
     const cleanedNumber = input.replace(/[^\d\(\)\-\s]/g, "");
     const formartedNumber = formatPhoneNumber(cleanedNumber);
 
-    setNumber(formartedNumber);
+    // setNumber(formartedNumber);
+    setValue("phone", formartedNumber);
   };
 
   const handleOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -35,21 +47,28 @@ export const PhoneNumberInput = ({}: PhoneNumberInputProps) => {
       (number.endsWith("(") || number.endsWith(")") || number.endsWith("-"))
     ) {
       e.preventDefault();
-      setNumber(number.slice(0, -1));
+      // setNumber(number.slice(0, -1));
+      setValue("phone", number.slice(0, -1));
     }
   };
 
   return (
-    <input
-      required
-      type="tel"
-      title="Digite seu número de telefone"
-      placeholder="Telefone"
-      className="w-full p-3 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 dark:bg-gray-900 dark:text-gray-200 dark:border-black dark:focus:ring-fusion-cyan"
-      value={number}
-      maxLength={15}
-      onChange={handleTypePhoneNumber}
-      onKeyDown={handleOnKeyDown}
-    />
+    <>
+      <input
+        {...register("phone")}
+        type="tel"
+        title="Digite seu número de telefone"
+        placeholder="Telefone"
+        className="w-full p-3 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 dark:bg-gray-900 dark:text-gray-200 dark:border-black dark:focus:ring-fusion-cyan"
+        maxLength={15}
+        onChange={handleTypePhoneNumber}
+        onKeyDown={handleOnKeyDown}
+      />
+      {errors.phone && (
+        <span className="text-red-500 text-sm">
+          {errors.phone?.message as string}
+        </span>
+      )}
+    </>
   );
 };
