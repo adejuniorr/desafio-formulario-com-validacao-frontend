ee73e17c20757489b0743f70bbcc178b2a863eb7
Ademir Júnior <adejr0220@gmail.com>
2024-10-22 14:19:21 -0300

    :recycle: refact NameInput
    Apply react-hook-form configs to component

--- src/components/form/NameInput.tsx
+++ src/components/form/NameInput.tsx
@@ -1,13 +1,23 @@
-interface NameInputProps {}
+import { useFormContext } from "react-hook-form";
 
-export const NameInput = ({}: NameInputProps) => {
+export const NameInput = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
   return (
-    <input
-      required
-      type="text"
-      title="Digite seu nome"
-      placeholder="Nome completo"
-      className="w-full p-3 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 dark:bg-gray-900 dark:text-gray-200 dark:border-black dark:focus:ring-fusion-cyan"
-    />
+    <>
+      <input
+        {...register("name")}
+        type="text"
+        title="Digite seu nome"
+        placeholder="Nome completo"
+        className="w-full p-3 border rounded-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 dark:bg-gray-900 dark:text-gray-200 dark:border-black dark:focus:ring-fusion-cyan"
+      />
+      {errors.name && (
+        <span className="text-red-500 text-sm">{errors.name?.message as string}</span>
+      )}
+    </>
   );
 };
