30d64c86c399919248a794941dc0d20b4bfa2102
Ademir Júnior <adejr0220@gmail.com>
2024-10-22 14:19:45 -0300

    :arrow-up: update Form
    Apply react-hook-form and zod libs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,28 +5,59 @@ import { PhoneNumberInput } from "./form/PhoneNumberInput";
 import { RoleSelector } from "./form/RoleSelector";
 import { SocialMediaInput } from "./form/SocialMediaInput";
 
-interface FormProps {}
+import { useForm, FormProvider } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
-export const Form = ({}: FormProps) => {
+const schema = z.object({
+  name: z.string().min(1, "Você deve informar o seu nome completo"),
+  email: z.string().email("E-mail inválido"),
+  phone: z.string().min(14, "Número de telefone inválido"),
+  role: z.string().min(1, "Você deve escolher um cargo de preferência"),
+  socialMediaLinks: z.array(z.string()).optional(),
+  socialMediaLinksValues: z.array(z.string().url("Link inválido")).optional(),
+});
+
+export const Form = () => {
+  const methods = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+      role: "",
+      socialMediaLinks: [],
+      socialMediaLinksValues: [],
+    },
+  });
+
+  const { handleSubmit } = methods;
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <form className="bg-fusion-dark-blue rounded-md shadow shadow-gray-700 max-w-[500px] w-full flex flex-col gap-4 px-4 py-6 mb-10">
-      <h1 className="font-extrabold text-[2rem] mb-4">
-        Preencha com seus dados
-      </h1>
-      <NameInput />
-      <EmailInput />
-      <PhoneNumberInput />
-      <RoleSelector />
-      <hr />
-      <p>Gostaria de adicionar alguma rede?</p>
-      <SocialMediaInput />
-      <span className="flex justify-end">
-        <Button
-          children="Enviar"
-          type="submit"
-          title="Clique para enviar os dados"
-        />
-      </span>
-    </form>
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-fusion-dark-blue rounded-md shadow shadow-gray-700 max-w-[500px] w-full flex flex-col gap-4 px-4 py-6 mb-10">
+        <h1 className="font-extrabold text-[2rem] mb-4">
+          Preencha com seus dados
+        </h1>
+        <NameInput />
+        <EmailInput />
+        <PhoneNumberInput />
+        <RoleSelector />
+        <hr />
+        <p>Gostaria de adicionar alguma rede?</p>
+        <SocialMediaInput />
+        <span className="flex justify-end">
+          <Button
+            children="Enviar"
+            type="submit"
+            title="Clique para enviar os dados"
+          />
+        </span>
+      </form>
+    </FormProvider>
   );
 };
